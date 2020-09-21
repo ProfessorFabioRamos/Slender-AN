a189483f437a80a01095e7f6739e1ea492959fe9
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-09-21 18:32:15 -0300

    Primeiro Commit

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool isOpen= false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.name == "Player" && !isOpen){
+            int objs = other.gameObject.GetComponent<Player>().objects;
+            if(objs ==  8){
+                transform.parent.GetComponent<Animation>().Play();
+                isOpen = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int objects = 0;
+    public Transform waterTransform;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(transform.position.y < waterTransform.position.y)
+            RenderSettings.fog = true;
+        else    
+           RenderSettings.fog = false; 
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "Collectible"){
+            //objects = objects+1;
+            //objects += 1;
+            objects++;
+            Destroy(other.gameObject);
+        }   
+    }
+}
