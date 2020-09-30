454b6aed7de33628d5245d30dc4b197f9715cef3
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-09-30 20:54:41 -0300

    28-09-20

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Door : MonoBehaviour
 {
     private bool isOpen= false;
+    public GameObject feedbackPanel;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        feedbackPanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,10 +23,19 @@ public class Door : MonoBehaviour
     void OnTriggerEnter(Collider other){
         if(other.gameObject.name == "Player" && !isOpen){
             int objs = other.gameObject.GetComponent<Player>().objects;
+            feedbackPanel.SetActive(true);
+            feedbackPanel.GetComponentInChildren<Text>().text = "Você possui "+objs+"/8 caveiras";
             if(objs ==  8){
+                feedbackPanel.SetActive(false);
                 transform.parent.GetComponent<Animation>().Play();
                 isOpen = true;
             }
+        }
+    }
+
+     void OnTriggerExit(Collider other){
+        if(other.gameObject.name == "Player" && !isOpen){  
+            feedbackPanel.SetActive(false);
         }
     }
 }
