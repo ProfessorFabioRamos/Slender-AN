3f554d77468a94ff3a4dce43e93f3eaf56e5ea07
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-10-05 18:37:51 -0300

    30-09-20

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public int objects = 0;
     public Transform waterTransform;
 
+    public GameObject grabShotgunPanel;
+    public GameObject shotgun;
+
+    //private bool canGrab=false;
+
     // Start is called before the first frame update
     void Start(){
-        
+        grabShotgunPanel.SetActive(false);
+        shotgun.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,6 +33,28 @@ public class Player : MonoBehaviour
             //objects += 1;
             objects++;
             Destroy(other.gameObject);
-        }   
+        } 
+        if(other.gameObject.tag == "Gun"){
+            grabShotgunPanel.SetActive(true);
+            grabShotgunPanel.GetComponentInChildren<Text>().text = "Pressione 'E' para pegar";
+            //canGrab=true;
+        } 
+    }
+
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.tag == "Gun"){
+            grabShotgunPanel.SetActive(false);
+            //canGrab = false;
+        }
+    }
+
+    void OnTriggerStay(Collider other){
+        if(other.gameObject.tag == "Gun"){
+            if(Input.GetKeyDown(KeyCode.E)){
+                Destroy(other.gameObject);
+                grabShotgunPanel.SetActive(false);
+                shotgun.SetActive(true);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Shotgun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+    public float range = 8;
+    private Camera fpsCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       fpsCam = transform.GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            RaycastHit hit;
+
+            Vector3 origin = fpsCam.ViewportToWorldPoint(new Vector3(0,0,0));
+
+            if(Physics.Raycast(origin, fpsCam.transform.forward, out hit, range)){
+                //Debug.Log(hit.transform.name);
+                if(hit.transform.tag == "Enemy"){
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}
